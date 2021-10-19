65d51ac7b804dbcb11815d9e0f8ee251a5094634
Hayden Westphal <92334428+itshdog@users.noreply.github.com>
2021-10-19 14:17:00 -0500

    Add files via upload
    Uploading current website files

---
+++ Website/JS/blur.js
@@ -0,0 +1,23 @@
+function contactBlur() {
+    var x = document.getElementById("contactWrapper");
+    x.className = "hidden"
+    x.style.opacity = "0";
+}
+
+function contactOpen() {
+    var x = document.getElementById("contactWrapper");
+    x.className = ""
+    x.style.opacity = "1";
+}
+
+function themeBlur() {
+    var x = document.getElementById("themeWrapper");
+    x.className = "hidden"
+    x.style.opacity = "0";
+}
+
+function themeOpen() {
+    var x = document.getElementById("themeWrapper");
+    x.className = ""
+    x.style.opacity = "1";
+}
---
+++ Website/JS/scroll.js
@@ -0,0 +1,5 @@
+function scrollToTop() {
+    var x = document.getElementById("title");
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
