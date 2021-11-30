bb5dbc59e6dea28493e9ab25537d10ecfcbdfe3a
Hayden Westphal <92334428+itshdog@users.noreply.github.com>
2021-11-30 15:52:06 -0600

    v1.0.1 // Resizing
    This update is just to clean up the overall flow of the website.
    Resizing text and elements to fit better from 4k through mobile.

--- Website/JS/social.js
+++ Website/JS/social.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     var obj = document.getElementById("cssTheme");
     var x = localStorage.getItem("theme");
+    if (localStorage.getItem("theme") == null) {
+        var x = "themes/classic.css";
+    }
     obj.setAttribute("href", x);
 }
