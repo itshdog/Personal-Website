320a78de95d6005c1ff890783570a5196b1a5446
itshdog <haydenwestphal@gmail.com>
2022-01-29 16:44:19 -0600

    Combined JavaScript

--- Website/JS/scroll.js
+++
@@ -1,5 +0,0 @@
-function scrollToTop() {
-    var x = document.getElementById("title");
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-}
--- Website/JS/social.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var obj = document.getElementById("cssTheme");
-    var x = localStorage.getItem("theme");
-    if (localStorage.getItem("theme") == null) {
-        var x = "themes/classic.css";
-    }
-    obj.setAttribute("href", x);
-}
