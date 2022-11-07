0968fa3a6eb766350ed72d0bb133e2542eb57cb9
itshdog <haydenwestphal@gmail.com>
2022-11-07 11:20:36 -0600

    v1.3.4

--- JS/app.js
+++ JS/app.js
@@ -2,8 +2,6 @@
 //     await tsParticles.load("tsparticles", {
 //         preset: "fountain",
 //     });
-//   })();
-
 
 tsParticles.load("tsparticles", {
     particles: {
@@ -27,3 +25,43 @@ tsParticles.load("tsparticles", {
     },
     preset: "fountain",
 });
+
+function updateParticles(value) {
+    tsParticles.load("tsparticles", {
+        particles: {
+            number: {
+                value: value
+            },
+            links: {
+                enable: true,
+                opacity: 0.5,
+                distance: 100
+            },
+            move: {
+                enable: true
+            },
+            opacity: {
+                value: { min: 0.2, max: 0.3 }
+            },
+            size: {
+                value: { min: 2, max: 4 }
+            },
+        },
+    });
+    document.getElementById("pauseParticlesButton").className = "fas fa-circle selected";
+}
+
+function pauseParticles() {
+    var item = document.getElementById("pauseParticles");
+    var button = document.getElementById("pauseParticlesButton");
+
+    if (item.className == "settings-entry play") {
+        tsParticles.domItem(0).pause();
+        button.className = "fas fa-circle";
+        item.className = "settings-entry";
+    } else if (item.className == "settings-entry") {
+        tsParticles.domItem(0).play();
+        item.className = "settings-entry play";
+        button.className = "fas fa-circle selected";
+    }
+}
